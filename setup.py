afeb29d9623f1ad4f7ad46ce53c1d5011b4f0fc1
fily-gif <zeroad69@gmail.com>
2024-12-03 09:04:16 -0500

    testt

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
-
+# trigger gh actions
 setup(
     name='yacpl',
 	version='1.0.4',
