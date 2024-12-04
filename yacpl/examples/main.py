d05e891872d11d10fa9e9ace9bfd3f64eb7159bf
fily-gif <zeroad69@gmail.com>
2024-12-03 20:33:03 -0500

    the example file stopped working out of nowhere.

--- yacpl/examples/main.py
+++ yacpl/examples/main.py
@@ -1,5 +1,14 @@
 from yacpl import Yacpl, Color
+from time import sleep as sl
 
 yacpl = Yacpl()
 
 yacpl("Hello, World!", fg=Color.RED, bg=Color.WHITE)
+sl(1)
+yacpl("this is a test", fg=Color.YELLOW, bg=Color.BLUE)
+sl(0.5)
+yacpl("made in pure python", fg=Color.BLUE, bg=Color.MAGENTA)
+sl(0.4)
+yacpl("...with no external dependencies!", fg=Color.MAGENTA, bg=Color.CYAN)
+sl(0.4)
+yacpl("...for Hackclub High Seas!", fg=Color.CYAN, bg=Color.GREEN)
