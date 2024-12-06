6390e3d5e0da34b7983546c73a4598cd0ff750e6
fily-jpg <zeroad69@gmail.com>
2024-12-06 17:15:03 -0500

    oh hey it works now!

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup, find_packages
 from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
-#print(find_packages(where='yacpl'))
+#print(find_packages())
 setup(
     name='yacpl',
 	version='1.0.8',
-    packages=find_packages(where='yacpl'),
+    packages=find_packages(),
 	package_dir={'': '.'},
     long_description=long_description,
     long_description_content_type='text/markdown'
