0a0804708d5333b501bb2f036e964ea90ce03f6a
fily-jpg <zeroad69@gmail.com>
2024-12-22 21:54:32 -0500

    okay i have no idea how to make this. delimeters are hard. help. (emptied all files)

--- examples/main.py
+++ examples/main.py
@@ -1,16 +1,4 @@
 import yacpl
-print(yacpl.__file__)
-from yacpl import Yacpl, Color
-from time import sleep as sl
+yacpl = yacpl.Printer()
 
-yacpl = Yacpl()
-
-yacpl("Hello, World!", fg=Color.RED, bg=Color.WHITE)
-sl(1)
-yacpl("this is a test", fg=Color.YELLOW, bg=Color.BLUE)
-sl(0.5)
-yacpl("made in pure python", fg=Color.BLUE, bg=Color.MAGENTA)
-sl(0.4)
-yacpl("...with no external dependencies!", fg=Color.MAGENTA, bg=Color.CYAN)
-sl(0.4)
-yacpl("...for Hackclub High Seas!", bg=Color.GREEN)
+yacpl("&an;test&r; &ku;text&r;")
